6e5782fdf99072c9428356ac3c8245e32ea6bbd4
hugolpz <hugo.lpz@gmail.com>
2025-06-23 08:08:50 +0200

    update index

--- js/data/lessons.js
+++ js/data/lessons.js
@@ -1,11 +1,20 @@
 var lessons = [
   {
+    lesson: "00",
+    section: "A",
+    titleKor: "시험 siheom",
+    titleFra: "Lexique basique du Taekwondo",
+    guidelineFra: "1. Étudier le vocabulaire ; 3. Partiquer.",
+    image: [ "https://commons.wikimedia.org/wiki/Special:FilePath/Taekwondo_vocabulary-en.png" ]
+  },
+  {
     lesson: "01",
     section: "A",
     titleKor: "비디오 bidio",
     titleFra: "Vidéo",
     guidelineFra: "1. Suivre le rythme ; 2. Identifier les sections ; 3. Pratiquer l'enchainement.",
-    video: "https://www.youtube.com/embed/pcBdDKaw35Q?start=17&end=47&list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7"
+    video: "https://www.youtube.com/embed/pcBdDKaw35Q?start=17&end=47&list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7",
+    video2: "https://www.youtube.com/watch?v=6rdB_EfCosM"
   },
   {
     lesson: "01",
@@ -29,7 +38,8 @@ var lessons = [
     titleKor: "비디오 bidio",
     titleFra: "Vidéo",
     guidelineFra: "1. Suivre le rythme ; 2. Identifier les sections ; 3. Pratiquer l'enchainement.",
-    video: "https://www.youtube.com/embed/jZiFWTuXS8Y?start=10&list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7"
+    video: "https://www.youtube.com/embed/jZiFWTuXS8Y?start=10&list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7",
+    video2: "https://www.youtube.com/watch?v=1MKfAXX-MEo",
   },
   {
     lesson: "02",
@@ -61,7 +71,8 @@ var lessons = [
     titleKor: "비디오 bidio",
     titleFra: "Vidéo",
     guidelineFra: "1. Suivre le rythme ; 2. Identifier les sections ; 3. Pratiquer l'enchainement.",
-    video: "https://www.youtube.com/embed/rLjNmmQRS1U?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7"
+    video: "https://www.youtube.com/embed/rLjNmmQRS1U?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7",
+    video2: "https://www.youtube.com/watch?v=LD0ozzUhsgE",
   },
   {
     lesson: "03",
@@ -85,7 +96,8 @@ var lessons = [
     titleKor: "비디오 bidio",
     titleFra: "Vidéo",
     guidelineFra: "1. Suivre le rythme ; 2. Identifier les sections ; 3. Pratiquer l'enchainement.",
-    video: "https://www.youtube.com/embed/_IV8AS9GqRA?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7"
+    video: "https://www.youtube.com/embed/_IV8AS9GqRA?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7",
+    video2:  "https://www.youtube.com/watch?v=NzEK67-bcJo",
   },
   {
     lesson: "04",
@@ -109,7 +121,8 @@ var lessons = [
     titleKor: "비디오 bidio",
     titleFra: "Vidéo",
     guidelineFra: "1. Suivre le rythme ; 2. Identifier les sections ; 3. Pratiquer l'enchainement.",
-    video: "https://www.youtube.com/embed/FIje5o7KJVQ?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7"
+    video: "https://www.youtube.com/embed/FIje5o7KJVQ?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7",
+    video2: "https://www.youtube.com/watch?v=_lhyXM8dcWs",
   },
   {
     lesson: "05",
@@ -133,7 +146,8 @@ var lessons = [
     titleKor: "비디오 bidio",
     titleFra: "Vidéo",
     guidelineFra: "1. Suivre le rythme ; 2. Identifier les sections ; 3. Pratiquer l'enchainement.",
-    video: "https://www.youtube.com/embed/BzYkTauSLK0?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7"
+    video: "https://www.youtube.com/embed/BzYkTauSLK0?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7",
+    video2: "https://www.youtube.com/watch?v=ShYQhE6OHWI",
   },
   {
     lesson: "06",
@@ -141,7 +155,7 @@ var lessons = [
     titleKor: "분해 bunhae",
     titleFra: "Décomposition",
     guidelineFra: "1. Identifier les sections ; 2. Pratiquer l'enchainement.",
-    image: [ "https://commons.wikimedia.org/wiki/Special:FilePath/Taegeuk_Yuk_Jang.png" ]
+    image: [ "https://commons.wikimedia.org/wiki/Special:FilePath/Taegeuk_Yook_Jang.png" ]
   },
   {
     lesson: "06",
@@ -157,7 +171,8 @@ var lessons = [
     titleKor: "비디오 bidio",
     titleFra: "Vidéo",
     guidelineFra: "1. Suivre le rythme ; 2. Identifier les sections ; 3. Pratiquer l'enchainement.",
-    video: "https://www.youtube.com/embed/7rXHdRkKg-0?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7"
+    video: "https://www.youtube.com/embed/7rXHdRkKg-0?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7",
+    video2: "https://www.youtube.com/watch?v=r1G771ho8C8", // counting
   },
   {
     lesson: "07",
@@ -181,7 +196,8 @@ var lessons = [
     titleKor: "비디오 bidio",
     titleFra: "Vidéo",
     guidelineFra: "1. Suivre le rythme ; 2. Identifier les sections ; 3. Pratiquer l'enchainement.",
-    video: "https://www.youtube.com/embed/M78aSpAxMvc?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7"
+    video: "https://www.youtube.com/embed/M78aSpAxMvc?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7",
+    video2: "https://www.youtube.com/watch?v=8DfH9ahqrvQ"
   },
   {
     lesson: "08",
@@ -206,6 +222,7 @@ var lessons = [
     titleFra: "Vidéo",
     guidelineFra: "1. Suivre le rythme ; 2. Identifier les sections ; 3. Pratiquer l'enchainement.",
     video: "https://www.youtube.com/embed/3pVsu7SInGw?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7",
+    video2: "https://www.youtube.com/watch?v=LiO6AX9mTko&pp=0gcJCd4JAYcqIYzv",
     local: "images/태백 후면버전 [그대로 따라 하기].mp4"
   },
   {
@@ -231,6 +248,7 @@ var lessons = [
     titleFra: "Vidéo",
     guidelineFra: "1. Suivre le rythme ; 2. Identifier les sections ; 3. Pratiquer l'enchainement.",
     video: "https://www.youtube.com/embed/eqs95ICFdmY?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7",
+    video2: "https://www.youtube.com/watch?v=854ACHWnbEI",
     local: "images/금강 후면버전 [그대로 따라 하기].mp4"
   },
   {
@@ -256,6 +274,7 @@ var lessons = [
     titleFra: "Vidéo",
     guidelineFra: "1. Suivre le rythme ; 2. Identifier les sections ; 3. Pratiquer l'enchainement.",
     video: "https://www.youtube.com/embed/HW3DjYNn1Dk?list=PLs51UE_WmSzZ4zR38PUtTJrRxJCJQRxn7",
+    video2: "https://www.youtube.com/watch?v=0W948Q71Edc",
     local: "images/고려 후면버전 [그대로 따라 하기].mp4"
   },
   {
